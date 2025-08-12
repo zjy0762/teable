252eaa06958f81c9cd06ab0246a09b6441c9fd70
nichenqin <nichenqin@hotmail.com>
2025-08-12 11:45:38 +0800

    fix: fix formula reference a lookup

--- apps/nestjs-backend/src/db-provider/sqlite.provider.ts
+++ apps/nestjs-backend/src/db-provider/sqlite.provider.ts
@@ -8,6 +8,7 @@ import type {
   IGeneratedColumnQueryInterface,
   ILookupOptionsVo,
   ISelectQueryInterface,
+  ISelectFormulaConversionContext,
   ISortItem,
 } from '@teable/core';
 import {
@@ -630,7 +631,10 @@ ORDER BY
     return new SelectQuerySqlite();
   }
 
-  convertFormulaToSelectQuery(expression: string, context: IFormulaConversionContext): string {
+  convertFormulaToSelectQuery(
+    expression: string,
+    context: ISelectFormulaConversionContext
+  ): string {
     try {
       const selectQuery = this.selectQuery();
       selectQuery.setContext(context);
