2c27c59acad43e718e36cdca7750502cf0b62410
nichenqin <nichenqin@hotmail.com>
2025-08-18 18:03:37 +0800

    fix: fix record link select test

--- apps/nestjs-backend/src/features/aggregation/aggregation.service.ts
+++ apps/nestjs-backend/src/features/aggregation/aggregation.service.ts
@@ -571,7 +571,13 @@ export class AggregationService implements IAggregationService {
     }
 
     if (filterLinkCellSelected) {
-      await this.recordService.buildLinkSelectedQuery(qb, tableId, alias, filterLinkCellSelected);
+      await this.recordService.buildLinkSelectedQuery(
+        qb,
+        tableId,
+        dbTableName,
+        alias,
+        filterLinkCellSelected
+      );
     }
 
     return this.getRowCount(this.prisma, qb);
