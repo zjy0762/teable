3e00d3aa20b9e5a1057eb7901054caa4994b86ad
nichenqin <nichenqin@hotmail.com>
2025-08-23 13:32:35 +0800

    fix: try to fix lookup flatten

--- apps/nestjs-backend/src/features/field/field-calculate/field-converting.service.ts
+++ apps/nestjs-backend/src/features/field/field-calculate/field-converting.service.ts
@@ -484,7 +484,10 @@ export class FieldConvertingService {
       >(nativeSql.sql, ...nativeSql.bindings);
 
     for (const row of result) {
-      const oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]) as string[];
+      let oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]) as string[];
+      if (field.isLookup) {
+        oldCellValue = oldCellValue.flat(Infinity) as string[];
+      }
       const newCellValue = oldCellValue.reduce<string[]>((pre, value) => {
         // if key not in updatedChoiceMap, we should keep it
         if (!(value in updatedChoiceMap)) {
@@ -538,7 +541,10 @@ export class FieldConvertingService {
       >(nativeSql.sql, ...nativeSql.bindings);
 
     for (const row of result) {
-      const oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]) as string;
+      let oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]) as string;
+      if (field.isLookup && Array.isArray(oldCellValue)) {
+        oldCellValue = oldCellValue[0] as string;
+      }
 
       opsMap[row.__id] = [
         RecordOpBuilder.editor.setRecord.build({
@@ -617,7 +623,10 @@ export class FieldConvertingService {
       >(nativeSql.sql, ...nativeSql.bindings);
 
     for (const row of result) {
-      const oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]) as number;
+      let oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]) as number;
+      if (field.isLookup && Array.isArray(oldCellValue)) {
+        oldCellValue = oldCellValue[0] as number;
+      }
 
       opsMap[row.__id] = [
         RecordOpBuilder.editor.setRecord.build({
@@ -663,7 +672,11 @@ export class FieldConvertingService {
       .$queryRawUnsafe<{ __id: string; [dbFieldName: string]: string }[]>(nativeSql.toQuery());
 
     for (const row of result) {
-      const oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]);
+      let oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]);
+      if (field.isLookup && Array.isArray(oldCellValue)) {
+        oldCellValue = oldCellValue.flat(Infinity) as string[];
+      }
+
       let newCellValue;
 
       if (field.isMultipleCellValue && !Array.isArray(oldCellValue)) {
@@ -710,7 +723,10 @@ export class FieldConvertingService {
       .txClient()
       .$queryRawUnsafe<{ __id: string; [dbFieldName: string]: string }[]>(nativeSql.toQuery());
     for (const row of result) {
-      const oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]);
+      let oldCellValue = field.convertDBValue2CellValue(row[field.dbFieldName]);
+      if (field.isLookup && Array.isArray(oldCellValue)) {
+        oldCellValue = oldCellValue.flat(Infinity) as string[];
+      }
       opsMap[row.__id] = [
         RecordOpBuilder.editor.setRecord.build({
           fieldId: field.id,
