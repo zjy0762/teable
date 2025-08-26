b8d35ff8b2601dc3ecfeb2143ac7ef5eba68e0d7
nichenqin <nichenqin@hotmail.com>
2025-08-26 11:40:21 +0800

    fix: fix null cast in lookup multiple value

--- apps/nestjs-backend/src/features/record/record.service.ts
+++ apps/nestjs-backend/src/features/record/record.service.ts
@@ -1318,8 +1318,6 @@ export class RecordService {
     );
     const nativeQuery = queryBuilder.whereIn('__id', recordIds).toQuery();
 
-    this.logger.debug('getSnapshotBulkInner query: %s', nativeQuery);
-
     const result = await this.prismaService
       .txClient()
       .$queryRawUnsafe<
